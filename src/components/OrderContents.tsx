c525061c8963e2926bcbe5e88cef823e030879b7
ArielEzequielPerez <ariperez@frba.utn.edu.ar>
2025-05-03 15:28:36 -0300

    centra button

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -10,19 +10,29 @@ export default function OrderContents({ order }: OrderContentsProps) {
       <h2 className="font-black text-4xl"> Consumo </h2>
       <div className="space-y-3 mt-5">
         {order.length === 0 ? (
-          <p className="text-center">Ta' vacio</p>
+          <p className="text-center">La orden esta vacia</p>
         ) : (
-          order.map( item => (
-            <div key= {item.id}>
-              <p className="text-lg font-bold">
-                {item.name}{" "}
-                <span className="font-normal">x{item.quantity}</span>
-              </p>
-              <p className="text-lg font-bold">
-                {formatCurrency(item.price * item.quantity)}
-              </p>
-            </div>
-          ))
+          <div className="space-y-3 mt-10">
+            {order.map((item) => (
+              <div
+                key={item.id}
+                className="flex justify-between items-center border-t border-grey-200 py-5 last-of-type:border-b"
+              >
+                <div>
+                  <p className="text-lg">
+                    {item.name} - {formatCurrency(item.price)}
+                  </p>
+                  <p className="font-black">
+                    Cantidad: {item.quantity} -{" "}
+                    {formatCurrency(item.price * item.quantity)}
+                  </p>
+                </div>
+                <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
